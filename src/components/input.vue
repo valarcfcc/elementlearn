5d5461d76e59f1da98766b483c7797c6c4de12bf
c f <chengfan001@hotmail.com>
2019-12-05 19:17:01 +0800

    1

--- src/components/input.vue
+++ src/components/input.vue
@@ -3,13 +3,25 @@
     <el-button @click="showB" type="success">test</el-button>
     <input v-model="message" placeholder="edit me" />
     <p>Message is: {{ message }}</p>
+    <el-form :model="ruleForm">
+      <el-row>
+        <el-form-item label="时间" prop="testdate">
+          <el-date-picker
+            v-model="ruleForm.testdate"
+            type="datetime"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            format="yyyy-MM-dd HH:mm:ss"
+          ></el-date-picker>
+        </el-form-item>
+      </el-row>
+    </el-form>
     <el-date-picker
-      v-model="value1"
+      v-model="ruleForm.testdate"
       value-format="yyyy-MM-dd HH:mm:ss"
       type="datetime"
       placeholder="选择日期时间"
     ></el-date-picker>
-    <p>Date is: {{ value1 }}</p>
+    <p>Date is: {{ ruleForm.testdate }}</p>
   </div>
 </template>
 
@@ -20,7 +32,10 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       message: "",
-      value1: ''
+      ruleForm: {
+        testdate: "",
+        value: ""
+      }
     }
   },
   methods: {
