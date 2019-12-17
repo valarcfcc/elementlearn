0ea60c27e48bf44142e245754125bbfd48fef1f4
c f <chengfan001@hotmail.com>
2019-12-17 08:59:10 +0800

    1

--- src/components/input.vue
+++ src/components/input.vue
@@ -7,70 +7,96 @@
       <DatePicker type="date" placeholder="Select date" style="width: 200px"></DatePicker>
     </el-col>
     <el-col span="12">
-      <DatePicker type="daterange" placement="bottom-end" placeholder="Select date" style="width: 200px"></DatePicker>
+      <DatePicker
+        type="daterange"
+        placement="bottom-end"
+        placeholder="Select date"
+        style="width: 200px"
+      ></DatePicker>
     </el-col>
     <el-form :model="ruleForm" :inline="true" :rules="rules" label-width="150px">
       <el-row>
         <el-form-item label="时间" prop="testdate">
-          <el-date-picker v-model="ruleForm.testdate" type="datetime" value-format="yyyy-MM-dd HH:mm:ss"
-            format="yyyy-MM-dd HH:mm:ss" style="width: 200px" placeholder="请选择时间"></el-date-picker>
+          <el-date-picker
+            v-model="ruleForm.testdate"
+            type="datetime"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            format="yyyy-MM-dd HH:mm:ss"
+            style="width: 200px"
+            placeholder="请选择时间"
+          ></el-date-picker>
         </el-form-item>
       </el-row>
     </el-form>
-    <el-date-picker v-model="ruleForm.testdate" value-format="yyyy-MM-dd HH:mm:ss" type="datetime" placeholder="选择日期时间">
-    </el-date-picker>
+    <el-date-picker
+      v-model="ruleForm.testdate"
+      value-format="yyyy-MM-dd HH:mm:ss"
+      type="datetime"
+      placeholder="选择日期时间"
+    ></el-date-picker>
     <p>Date is: {{ ruleForm.testdate }}</p>
+    <el-button type="success" @click="isNotEmpty()"></el-button>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-    data() {
-      return {
-        msg: 'Welcome to Your Vue.js App',
-        message: "",
-        ruleForm: {
-          testdate: "",
-          value: ""
-        },
-        rules: {
-          testdate: [{
-            required: true,
-            message: "请输入",
-            trigger: ["blur"]
-          }]
-        }
-      }
-    },
-    methods: {
-      showB() {
-        alert("hi");
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      message: "",
+      ruleForm: {
+        testdate: "",
+        value: ""
+      },
+      rules: {
+        testdate: [{
+          required: true,
+          message: "请输入",
+          trigger: ["blur"]
+        }]
       }
     }
+  },
+  methods: {
+    showB () {
+      alert("hi");
+    },
+    isNotEmpty (obj) {
+      obj = ses;
+      alert(this.isNotEmptycomput(obj));
+    },
+
+    isNotEmptycomput (obj) {
+      return typeof obj != 'undefined' && obj != null && obj != ""
+    }
+  },
+  computed: {
+
   }
+}
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1,
-  h2 {
-    font-weight: normal;
-  }
+h1,
+h2 {
+  font-weight: normal;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-  a {
-    color: #42b983;
-  }
-
+a {
+  color: #42b983;
+}
 </style>
