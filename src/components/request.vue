ba09a78c5a2dbd584cb5cad00e791d9159a81327
valar cf <chengfan001@hotmail.com>
2020-06-17 23:24:23 +0800

    c

--- src/components/request.vue
+++ src/components/request.vue
@@ -3,7 +3,7 @@
     <el-button @click="doPost()" type="success">test</el-button>
     <el-button @click="doPostString()" type="success">字符串</el-button>
     <el-button @click="show()" type="success">调用父组件方法</el-button>
-    <span>{{ this.message }}</span>
+    <span>{{ this.reqData.total }}</span>
   </div>
 </template>
 
@@ -13,13 +13,35 @@ export default {
   props: {
     user: Object
   },
+
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       // user: {
       //   name: "lim59"
       // },
+      json: {
+        id: "123",
+        labelname: "",
+        count: "",
+        recommend: "",
+        fans: ""
+      },
+      req: {
+        data: "",
+        pageNum: 1,
+        pageSize: 1,
+        sort: ""
+      },
+      reqData: {
+        list: [],
+        total: 1
+      }
     }
+  },
+  beforeMount () {
+    this.doPost()
+
   },
   methods: {
     show () {
@@ -27,18 +49,23 @@ export default {
       this.$emit('show');
     },
     doPost () {
+      this.req.data = JSON.stringify(this.json);
+      console.log(JSON.stringify(this.req));
       this.$axios({
-        url: '/api/user',
-        data: JSON.stringify(this.user),
+        url: '/api/tbLabel/page',
+        data: this.req,
         method: 'post',
         headers: {
           'Content-Type': 'application/json;charset=UTF-8'
         },
       })
         .then(function (response) {
+          let abc = JSON.parse(response.data.data);
+          console.log(abc);
+          console.log(abc.total);
+          this.reqData.total = 3;
 
-          alert(response.data);
-          console.log(response);
+
         })
         .catch(function (error) {
           console.log(error);
@@ -54,6 +81,7 @@ export default {
         },
       })
         .then(function (response) {
+
           console.log(response);
         })
         .catch(function (error) {
